bfc353ad34fa398048b5c7e9ddc647c02edf37f4
Perpy-del <perpetualmeninwa@gmail.com>
2023-05-04 17:02:51 +0100

    Started on styling chatjibiti side menu

--- chatjibiti/src/App.js
+++ chatjibiti/src/App.js
@@ -1,10 +1,18 @@
+import "./normalize.css";
+
 function App() {
   return (
-    <div className="App">
-      <aside>
-        <h1>Aside</h1>
+    // This is the main UI for the page
+    <div className="bg-[#282c34] absolute top-0 right-0 left-0 bottom-0 text-white flex text-center">
+      {/* This is for the left side menu  */}
+      <aside className="w-[260px] absolute top-0 bottom-0 left-0 bg-[#202123] p-[10px]">
+        <div className="p-[9px] border border-solid border-white rounded-[5px] text-start text-[0.8rem] flex items-center">
+          <span className="pl-[6px] pr-[12px] text-[1.5rem]">+</span>
+          New chat
+        </div>
       </aside>
-      <section></section>
+      {/* This is for the right side menu where the response is displayed */}
+      <section className="flex-1 bg-[#343541]"></section>
     </div>
   );
 }
--- chatjibiti/tailwind.config.js
+++ chatjibiti/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      "text-gray": "#D9D9E3",
+      "gray-dark": "#343541",
+      "gray-light": "#d3dce6",
+      // "bg-color-right": "#202123",
+      // "bg-color-left": "#282c34",
+      white: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
