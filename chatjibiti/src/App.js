bc33129f244aac84642569968f08fe3d7bc7f185
Perpy-del <perpetualmeninwa@gmail.com>
2023-05-04 12:09:14 +0100

    Removed unnecessary files from the template created by cra and installed tailwind css

--- chatjibiti/src/App.js
+++ chatjibiti/src/App.js
@@ -1,6 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
@@ -8,20 +5,6 @@ function App() {
         <h1>Aside</h1>
       </aside>
       <section></section>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
