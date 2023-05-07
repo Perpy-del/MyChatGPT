858399ef35904b5555ad297b730be2ce9a809e64
Perpy-del <perpetualmeninwa@gmail.com>
2023-05-07 21:35:17 +0100

    Added the sticky positions to the nav by the left for desktop and the input/footer on the bottom of the right

--- chatjibiti/src/App.js
+++ chatjibiti/src/App.js
@@ -14,7 +14,7 @@ function App() {
     // This is the main UI for the page
     <div className="text-white flex items-center text-center">
       {/* This is for the left side menu  */}
-      <aside className="md:flex md:flex-col md:w-[20%] md:absolute md:top-0 md:bottom-0 md:left-0 md:bg-[#202123] md:p-[10px] hidden">
+      <aside className="md:flex md:flex-col md:w-[20%]  md:top-0 md:bottom-0 md:left-0 md:bg-[#202123] md:p-[10px] hidden md:fixed">
         {/* For the New Chat Button/Box */}
         <div className="p-[8px] border border-solid border-[#fff3] rounded-[5px] text-start text-[0.8rem] flex items-center hover:bg-[rgba(255,255,255,0.1)] transition-all ease-linear duration-200 cursor-pointer">
           <span className="pl-[6px] pr-[12px] text-[1.5rem]">+</span>
@@ -46,16 +46,20 @@ function App() {
 
       {/* This is for the right side menu where the response is displayed */}
       <section className="flex flex-col bg-[#343541] absolute top-0 bottom-0 right-0 md:w-[80%] w-[100%] text-[#D9D9E3]">
-        {/* EXAMPLES, CAPABILITIES AND LIMITATIONS */}
+        {/* TOP HAMBURGER MENU FOR MOBILE/TABLETS */}
         <div className="flex justify-between items-center px-4 py-[10px] border-[#838181] border-b-[0.2px] md:hidden">
           <RxHamburgerMenu className="text-[1.4rem] cursor-pointer" />
           <h3 className="text-[0.9rem]">New Chat</h3>
           <AiOutlinePlus className="text-[1.4rem] cursor-pointer" />
         </div>
-        <h1 className="hidden md:block md:font-bold pb-10 text-[2.175rem] text-white">
+
+        {/* CHATGPT TEXT HIDDEN FOR MOBILE/TAB DEVICES */}
+        <h1 className="hidden md:block md:font-bold pb-10 text-[2.175rem] text-white pt-[7rem]">
           ChatGPT
         </h1>
-        <div className="flex gap-4 items-start w-[75%] mx-auto pb-[5rem]">
+
+        {/* EXAMPLES, CAPABILITIES AND LIMITATIONS */}
+        <div className="flex gap-4 items-start w-[75%] mx-auto pb-[10rem] bg-[#343541]">
           {/* EXAMPLES */}
           <div className="flex flex-col items-center gap-4 w-[30%]">
             <BsSun className="text-[1.4rem]" />
@@ -104,8 +108,8 @@ function App() {
             </h5>
           </div>
         </div>
-        <div className="w-[75%] mx-auto">
-          <div className="flex justify-between px-5 py-4 mb-[0.5rem] w-[95%] bg-[#40414f] text-sm">
+        <div className="flex flex-col items-center w-[75%] ml-5 fixed bottom-0">
+          <div className="flex justify-between px-5 py-4 mb-[0.5rem] w-[80%] bg-[#40414f] text-sm">
             <input
               type="text"
               placeholder="Send a message."
@@ -113,7 +117,7 @@ function App() {
             />
             <BsSend className="text-[#7f8196]" />
           </div>
-          <h6 className="text-[0.7rem] pt-2">
+          <h6 className="text-[0.7rem] pt-2 pb-6">
             Free Research Preview. ChatGPT may produce inaccurate information
             about people, places, or facts. <span> </span>
             <a
